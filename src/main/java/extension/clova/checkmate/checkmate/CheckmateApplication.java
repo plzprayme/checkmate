e5ddb811bbc176317a964e504ebf50a91e6d8122
prayme <dbfpzk142@gmail.com>
2022-01-04 14:59:18 +0900

    스프링 프로젝트 생성

---
+++ src/main/java/extension/clova/checkmate/checkmate/CheckmateApplication.java
@@ -0,0 +1,13 @@
+package extension.clova.checkmate.checkmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckmateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CheckmateApplication.class, args);
+    }
+
+}
