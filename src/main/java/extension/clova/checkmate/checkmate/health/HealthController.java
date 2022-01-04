593a610cebd9a8c87c9f29f4527f7b2dd9157c18
prayme <dbfpzk142@gmail.com>
2022-01-04 16:23:39 +0900

    chore: CD 파이프라인 추가

---
+++ src/main/java/extension/clova/checkmate/checkmate/health/HealthController.java
@@ -0,0 +1,13 @@
+package extension.clova.checkmate.checkmate.health;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+    @GetMapping("/health")
+    public ResponseEntity<Void> health() {
+        return ResponseEntity.ok().build();
+    }
+}
